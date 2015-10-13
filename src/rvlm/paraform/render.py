48c5da040867911dbd6cf7407cf5650067646232
Pavel Kretov <firegurafiku@gmail.com>
2015-10-13 17:32:37 +0300

    render.py: remove functions moved to 'rvlm-labhelpers'
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- src/rvlm/paraform/render.py
+++ src/rvlm/paraform/render.py
@@ -12,17 +12,7 @@ def render_to_bool_array(obj, ranges, nums):
     return vpf(xs, ys, zs)
 
 
-# TODO: Find proper way to do it
-def tpl(*xs):
-    return tuple(xs)
 
-
-def head(xs):
-    return xs[0]
-
-
-def tail(xs):
-    return xs[1:]
 
 
 def iterate_range(rng, num):
@@ -44,41 +34,6 @@ def iterate_range(rng, num):
     delta = (b - a) / (num - 1)
     for i in range(num):
         yield a + delta*i
-
-
-def iterate_ranges(ranges, nums):
-    """
-
-    :param ranges:
-    :param nums:
-    :return:
-
-        >>> from pprint import pprint
-        >>> pprint(list(iterate_ranges([(0, 1), (0, 1)], [2, 5])))
-        [(0.0, 0.0),
-         (0.0, 0.25),
-         (0.0, 0.5),
-         (0.0, 0.75),
-         (0.0, 1.0),
-         (1.0, 0.0),
-         (1.0, 0.25),
-         (1.0, 0.5),
-         (1.0, 0.75),
-         (1.0, 1.0)]
-
-
-    """
-    if len(ranges) <= 0:
-        return None
-    elif len(ranges) == 1:
-        yield from iterate_range(ranges[0], nums[0])
-    else:
-        (a, b), *tail_ranges = ranges
-        num,    *tail_nums   = nums
-
-        for x in iterate_range((a, b), num):
-            for rest in iterate_ranges(tail_ranges, tail_nums):
-                yield (x,) + (rest,)
 
 
 # TODO: Ad-hoc
