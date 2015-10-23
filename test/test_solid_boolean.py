0f0bd9f1d5c736f3c4d0732566df35630d5b526f
Pavel Kretov <firegurafiku@gmail.com>
2015-10-23 08:05:55 +0300

    test_solid_boolean.py: try to be nice with PyCharm's type checker
    Switch from (a == b).all() to numpy.all(a == b), see that nothing got
    better. Fix the revision to post a link to JetBrains bug tracker.
    
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- test/test_solid_boolean.py
+++ test/test_solid_boolean.py
@@ -1,36 +1,37 @@
 import os
-import numpy as np
+import numpy
 import rvlm.paraform.solid as pfs
 
 
 def test_solid_boolean_operations():
 
-    def render(obj):
-        grid = np.meshgrid(np.linspace(-2, 2, 50),
-                           np.linspace(-2, 2, 50),
-                           np.linspace(-2, 2, 50))
-        vpf = np.vectorize(obj.pf)
+    def render(obj) -> numpy.ndarray:
+        grid = numpy.meshgrid(numpy.linspace(-2, 2, 50),
+                              numpy.linspace(-2, 2, 50),
+                              numpy.linspace(-2, 2, 50))
+
+        vpf = numpy.vectorize(obj.pf)
         return vpf(*grid)
 
-    def load(name):
+    def load(name) -> numpy.ndarray:
         real_name = os.path.join(os.path.dirname(__file__), "renders", name)
-        with np.load(real_name) as f:
+        with numpy.load(real_name) as f:
             return f['arr_0']
 
     cube = pfs.Cuboid(2.0, 2.0, 2.0)
     ball = pfs.Ellipsoid(2.4, 2.4, 2.4)
 
     # Check mathematical properties.
-    assert (render(cube | cube) == render(cube)).all()
-    assert (render(cube & cube) == render(cube)).all()
-    assert render(cube ^ cube).any() == False
+    assert numpy.all(render(cube | cube) == render(cube))
+    assert numpy.all(render(cube & cube) == render(cube))
+    assert numpy.any(render(cube ^ cube)) == False
 
     # Compare to saved renders.
-    assert (render(cube & ball) == load("cube_and_ball.npz")).all()
-    assert (render(cube | ball) == load("cube_or_ball.npz")).all()
-    assert (render(cube ^ ball) == load("cube_xor_ball.npz")).all()
-    assert (render(cube - ball) == load("cube_minus_ball.npz")).all()
-    assert (render(ball - cube) == load("ball_minus_cube.npz")).all()
+    assert numpy.all(render(cube & ball) == load("cube_and_ball.npz"))
+    assert numpy.all(render(cube | ball) == load("cube_or_ball.npz"))
+    assert numpy.all(render(cube ^ ball) == load("cube_xor_ball.npz"))
+    assert numpy.all(render(cube - ball) == load("cube_minus_ball.npz"))
+    assert numpy.all(render(ball - cube) == load("ball_minus_cube.npz"))
 
 
 if __name__ == "__main__":
